55160265bd9eb9b357b8cd8511467acedb15b72f
Michael Lan <michaellan202@gmail.com>
2024-03-25 14:41:56 -0700

    more progress

--- lib/yabai.ml
+++ lib/yabai.ml
@@ -1,4 +1,5 @@
 open Ppx_yojson_conv_lib.Yojson_conv.Primitives
+open Containers
 
 module Appinfo = struct
   type t = { app : App.t; id : string; space : string }
@@ -10,16 +11,34 @@ module Query = struct
 end
 
 module State = struct
-  module AppMap = Map.Make (App)
+  module Locinfo = struct
+    type t = { id : string; space : string }
+  end
 
-  type r = { id : string; space : string }
-  type t = r list AppMap.t
+  type t = (App.t, Locinfo.t list) Hashtbl.t
 
-  let empty : t = AppMap.empty
+  let create () = Hashtbl.create 16
 
-  let t_of_query : Query.t -> t =
-    List.fold_left
-      (fun acc (x : Appinfo.t) ->
-        AppMap.add_to_list x.app { id = x.id; space = x.space } acc)
-      empty
+  let update (s : t) (q : Query.t) =
+    let tbl = Hashtbl.create 8 in
+    let id_to_space = Hashtbl.create 8 in
+    List.iter
+      (fun Appinfo.{ app; id; space } ->
+        let l = try Hashtbl.find tbl app with Not_found -> [] in
+        Hashtbl.replace tbl app (Locinfo.{ id; space } :: l);
+        Hashtbl.replace id_to_space id space)
+      q;
+
+    (* [tbl] now contains the set of windows that now exist *)
+    (* [id_to_space] now contains a mapping from window ID to space *)
+
+    (* keep all windows that continue to exist, processing *)
+    (* them first to keep their order. their space may have changed *)
+    Hashtbl.filter_map_inplace
+      (fun app infos ->
+        try
+          let now = Hashtbl.find tbl app in
+          Some (List.inter ~eq:Stdlib.( = ) infos now)
+        with Not_found -> None)
+      s
 end
